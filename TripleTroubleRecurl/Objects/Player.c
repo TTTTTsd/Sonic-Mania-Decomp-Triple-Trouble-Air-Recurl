c327b250c074f724f868126bbc7a21c6d1cc80fd
TTTTTsd <49044793+TTTTTsd@users.noreply.github.com>
2022-08-18 23:33:01 -0400

    Add files via upload

--- TripleTroubleRecurl/Objects/Player.c
+++ TripleTroubleRecurl/Objects/Player.c
@@ -8,7 +8,7 @@ void Player_State_Air_Hook(void) {
 	if (self->animator.animationID != ANI_ABILITY_0 && self->animator.animationID != ANI_ABILITY_1 && self->animator.animationID != ANI_ABILITY_2
             && self->animator.animationID != ANI_ABILITY_3
             && self->animator.animationID != ANI_ABILITY_4 && self->animator.animationID != ANI_ABILITY_5 && self->animator.animationID != ANI_ABILITY_6
-            && self->animator.animationID != ANI_ABILITY_7) {
+            && self->animator.animationID != ANI_ABILITY_7 && self->animator.animationID != ANI_STICK && self->animator.animationID != ANI_SKID) {
             if (self->jumpPress && self->animator.animationID != ANI_JUMP) {
                 self->applyJumpCap     = false;
                 self->jumpAbilityState = 1;
